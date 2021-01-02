70a49bc16b9e2a1d78c46c18ca3f06eb4a86f1af
armaanreza <syed.arizona@gmail.com>
2021-01-02 02:25:32 -0500

    initial commit

---
+++ handler.js
@@ -0,0 +1,40 @@
+const { DynamoDB } = require('aws-sdk');
+
+const db = new DynamoDB.DocumentClient();
+const TableName = process.env.TABLE_NAME;
+
+module.exports.create = async event => {
+    const newAnimal = {
+        name: event.body.name,
+        age: event.body.age,
+        family: event.body.family
+    };
+
+    await db.put({
+        TableName,
+        Item: newAnimal
+    }).promise()
+
+    return { statusCode: 200, body: JSON.stringify(newAnimal)};
+};
+
+module.exports.list = async event => {
+    const animals = await db.scan({
+        TableName,
+    }).promise()
+
+    return { statusCode: 200, body: JSON.stringify(animals)};
+};
+
+module.exports.delete = async event => {
+    const animalToBeRemovedName = event.pathParameters.name;
+
+    await db.delete({
+        TableName,
+        Key: {
+            name: animalToBeRemovedName
+        }
+    }).promise();
+
+    return { statusCode: 200};
+};
